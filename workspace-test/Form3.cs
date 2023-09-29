88b2aef4ebc3acac703982d79223a721fb2c3034
Walter Cheng <chengwaltj@gmail.com>
2023-09-29 14:14:25 -0700

    add weight to 2 walls

--- workspace-test/Form3.cs
+++ workspace-test/Form3.cs
@@ -14,12 +14,20 @@ namespace workspace_test
     public partial class Form3 : Form
     {
         private float wAdd = 0;
-        private float HP = 0;
-        private float HA = 0;
-        private float HB = 0;
-        private float HS = 0;
-        private float WW = 0;
+        private float HP1 = 0;
+        private float HA1 = 0;
+        private float HB1 = 0;
+        private float HS1 = 0;
+        private float WW1 = 0;
         private float LD = 0;
+
+        private float HP2 = 0;
+        private float HA2 = 0;
+        private float HB2 = 0;
+        private float HS2 = 0;
+        private float WW2 = 0;
+
+        private bool same = false;
 
         public Form3()
         {
@@ -30,18 +38,28 @@ namespace workspace_test
         {
             InitializeComponent();
             LD = paramLD;
-            textBox1.Text = HP.ToString();
-            textBox2.Text = HA.ToString();
-            textBox3.Text = HB.ToString();
-            textBox4.Text = HS.ToString();
-            textBox5.Text = WW.ToString();
+            textBox1.Text = HP1.ToString();
+            textBox2.Text = HA1.ToString();
+            textBox3.Text = HB1.ToString();
+            textBox4.Text = HS1.ToString();
+            textBox5.Text = WW1.ToString();
+
+            textBox6.Text = HP2.ToString();
+            textBox7.Text = HA2.ToString();
+            textBox8.Text = HB2.ToString();
+            textBox9.Text = HS2.ToString();
+            textBox10.Text = WW2.ToString();
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
             try
             {
-                HP = float.Parse(textBox1.Text);
+                HP1 = float.Parse(textBox1.Text);
+                if (same) {
+                    HP2 = HP1;
+                    textBox6.Text = HP2.ToString();
+                }
             }
             catch(FormatException)
             {
@@ -53,7 +71,12 @@ namespace workspace_test
         {
             try
             {
-                HA = float.Parse(textBox2.Text);
+                HA1 = float.Parse(textBox2.Text);
+                if (same)
+                {
+                    HA2 = HA1;
+                    textBox7.Text = HA2.ToString();
+                }
             }
             catch (FormatException)
             {
@@ -64,7 +87,12 @@ namespace workspace_test
         {
             try
             {
-                HB = float.Parse(textBox3.Text);
+                HB1 = float.Parse(textBox3.Text);
+                if (same)
+                {
+                    HB2 = HB1;
+                    textBox8.Text = HB2.ToString();
+                }
             }
             catch (FormatException)
             {
@@ -76,7 +104,12 @@ namespace workspace_test
         {
             try
             {
-                HS = float.Parse(textBox4.Text);
+                HS1 = float.Parse(textBox4.Text);
+                if (same)
+                {
+                    HS2 = HS1;
+                    textBox9.Text = HS2.ToString();
+                }
             }
             catch (FormatException)
             {
@@ -87,7 +120,98 @@ namespace workspace_test
         {
             try
             {
-                WW = float.Parse(textBox5.Text);
+                WW1 = float.Parse(textBox5.Text);
+                if (same)
+                {
+                    WW2 = WW1;
+                    textBox10.Text = WW2.ToString();
+                }
+            }
+            catch (FormatException)
+            {
+
+            }
+        }
+
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                HP2 = float.Parse(textBox6.Text);
+                if (same)
+                {
+                    HP1 = HP2;
+                    textBox1.Text = HP1.ToString();
+                }
+            }
+            catch (FormatException)
+            {
+
+            }
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                HA2 = float.Parse(textBox7.Text);
+                if (same)
+                {
+                    HA1 = HA2;
+                    textBox2.Text = HA1.ToString();
+                }
+            }
+            catch (FormatException)
+            {
+
+            }
+        }
+
+        private void textBox8_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                HB2 = float.Parse(textBox8.Text);
+                if (same)
+                {
+                    HB1 = HB2;
+                    textBox3.Text = HB1.ToString();
+                }
+            }
+            catch (FormatException)
+            {
+
+            }
+        }
+
+        private void textBox9_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                HA2 = float.Parse(textBox9.Text);
+                if (same)
+                {
+                    HA1 = HA2;
+                    textBox4.Text = HA1.ToString();
+                }
+            }
+            catch (FormatException)
+            {
+
+            }
+        }
+
+        private void textBox10_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                WW2 = float.Parse(textBox10.Text);
+                if (same)
+                {
+                    WW1 = WW2;
+                    textBox5.Text = WW1.ToString();
+                }
             }
             catch (FormatException)
             {
@@ -97,7 +221,11 @@ namespace workspace_test
 
         private void button1_Click(object sender, EventArgs e)
         {
-            wAdd = LD * WW * (0.5F * (HA + HB) + HP + HS);
+            float w1 = LD * WW1 * (0.5F * (HA1 + HB1) + HP1 + HS1);
+            float w2 = LD * WW2 * (0.5F * (HA2 + HB2) + HP2 + HS2);
+
+            wAdd = w1 + w2;
+
             this.DialogResult = DialogResult.OK;
             this.Close();
         }
@@ -105,6 +233,11 @@ namespace workspace_test
         public float GetWAdd()
         {
             return wAdd;
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            same = checkBox1.Checked;
         }
     }
 }
